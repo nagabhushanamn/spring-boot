0738208aca80a41063b8b95170bcaef47087de2b
nagabhushanamn <nagabhushanamn@gmail.com>
2016-10-26 12:13:25 +0530

    added variable 'v'

--- spring-boot-samples/spring-boot-sample-atmosphere/src/main/java/sample/atmosphere/SampleAtmosphereApplication.java
+++ spring-boot-samples/spring-boot-sample-atmosphere/src/main/java/sample/atmosphere/SampleAtmosphereApplication.java
@@ -42,6 +42,7 @@ public class SampleAtmosphereApplication {
 	@Bean
 	public EmbeddedAtmosphereInitializer atmosphereInitializer() {
 		int newVar;
+		int v;
 		return new EmbeddedAtmosphereInitializer();
 	}
 
